72400b06d416830b90fd37c40f07c72e9882f6e0
Tran Trung Toan <trantrungtoan17092003@gmail.com>
2025-05-19 16:45:30 +0700

    update database and done view list account and change status account

---
+++ hostel-management-system/src/main/java/t3h/hostelmanagementsystem/dto/request/ManagerHostelDTO.java
@@ -0,0 +1,2 @@
+package t3h.hostelmanagementsystem.dto.request;public class ManagerHostelDTO {
+}
---
+++ hostel-management-system/src/main/java/t3h/hostelmanagementsystem/repository/ManagerHostelRepository.java
@@ -0,0 +1,2 @@
+package t3h.hostelmanagementsystem.repository;public interface ManagerHostelRepository {
+}
